0dc93716412dedac536a2d1ba4021a65ee44b283
Kate Lysak <lykaterina27@gmail.com>
2023-11-26 18:35:38 -0500

    Lesson 2

---
+++ test.js
@@ -0,0 +1,25 @@
+var name = 'Piter';
+
+var string = `Hello my name is ${name}`; // comment is added
+
+console.log(string);
+console.log(string.length);
+console.log(string[0]);
+console.log(string[26]);
+
+console.log(5-'5');
+
+// num is a variable which takes str and converts to number 
+var str = '3';
+var num = parseInt(str);
+console.log(num);
+
+// num is a variable which takes decimal and converts to number with decimals
+var str2 = '7.14';
+var num2 = parseFloat(str2);
+console.log(num);
+
+
+/*
+multiline comment 
+*/
