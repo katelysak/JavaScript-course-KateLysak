c9058ce1735aab04b6a4488647334f448aba7e90
Kate Lysak <lykaterina27@gmail.com>
2023-11-28 22:08:13 -0500

    Home_work_part1

---
+++ Home_works/script.js
@@ -0,0 +1,30 @@
+// console.log('number' + 23 + 32);
+// console.log(41 + 1 + 'number');
+console.log(typeof(null + 1));    // YES - prints number 1 as null is empty and it's like 0 + 1 = 1
+// console.log('five' + + 'two');
+console.log(2 && 7)   // YES - prints 7 as both 2 and 7 are not 0, are both true and so, first 2 will be executed and then 7
+// console.log(+'40' + +'2');
+// console.log('10' - 5 === 6);
+// console.log(true + false);
+// console.log('4px' - 3)
+// console.log('4' - 3);
+// console.log('2' + 3 ** 2);
+// console.log(12 / '6');
+// console.log(23 + 42 + 'number');
+// console.log('10' + (5 === 6));
+// console.log('number' + 15 + 3);
+console.log(undefined + 1);    // prints NaN and it's impossible to get a value for the undefined and it's not declared yet
+// console.log('true' == true);
+console.log(false == 'false');    //NO // prints true because == means that we compare the values and not data types
+console.log(null == '')    // YES - prints false because comparing any variable with null will always evaluate to false, regardless if it's value
+// console.log(3 ** (9 / 3));
+// console.log(!!'false' == !!'true');
+// console.log(0 || '0' && 1)
+console.log(1 < 2 < 3);    // prints true as 1 < 2 -> true, 2 < 3 -> true; all statements are true
+// console.log('foo'+ + +'bar');
+// console.log(3 ** 2 / 3);
+console.log(1 < 2 > 3);   // prints false as 1 < 2 -> true, 2 > 3 -> false; not all statements are true
+// console.log((+null == false) < 1);
+// console.log(false && true || true);
+// console.log(false && (true || true));
+// console.log((+null == false) < 1 ** 5);
