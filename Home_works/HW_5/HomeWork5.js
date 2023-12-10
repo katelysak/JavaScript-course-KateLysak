f80282b0502cabe8df827fafe3d01262cd717caf
Kate Lysak <lykaterina27@gmail.com>
2023-12-10 18:37:35 -0500

    Updated Home work 5

--- Home_works/HW_5/HomeWork5.js
+++ Home_works/HW_5/HomeWork5.js
@@ -4,8 +4,8 @@
 const originalArray = [1, 2, 3, 4, 5];
 
 const reversedArray = arrayPassed => {
-    const revercedArray = arrayPassed.reverse();
-    return revercedArray;
+    const reverseArray = arrayPassed.reverse();
+    return reverseArray;
 }
 
 console.log(reversedArray(originalArray));     // returns [5, 4, 3, 2, 1]
